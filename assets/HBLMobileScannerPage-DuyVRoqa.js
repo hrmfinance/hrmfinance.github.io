69b2a444776aff5650c5006bd578b77a62e7e0ea
Hector Felix Ricardo Miranda <hectoricardom@gmail.com>
2026-04-29 21:01:06 -0400

    juhgy6f5df67g

---
+++ assets/HBLMobileScannerPage-DuyVRoqa.js
@@ -0,0 +1,1 @@
+import{e as a,a0 as n,Q as o}from"./index-DG_Aj2oh.js";const l=()=>a(n,{onScanComplete:e=>{o("Scan completed:",e)}});export{l as default};
--- assets/HBLMobileScannerPage-imuX6K4h.js
+++
@@ -1,1 +0,0 @@
-import{e as a,a0 as n,Q as o}from"./index-W_l-_0YJ.js";const l=()=>a(n,{onScanComplete:e=>{o("Scan completed:",e)}});export{l as default};
