ea0edd5c1e79f94baf07b8bb587d44f06c93f173
Hector Felix Ricardo Miranda <hectoricardom@gmail.com>
2026-06-02 13:07:30 -0400

    daDada

---
+++ assets/HBLMobileScannerPage-DSR1XZ8I.js
@@ -0,0 +1,1 @@
+import{e as n,$ as a,K as o}from"./index-SoR1p7_W.js";const l=()=>n(a,{onScanComplete:e=>{o("Scan completed:",e)}});export{l as default};
--- assets/HBLOfflineScannerPage-Di-ZDVOd.js
+++
@@ -1,1 +0,0 @@
-import{e,a0 as n}from"./index-BFdtPeVk.js";const r=()=>e(n,{});export{r as default};
